bbc2fb51206fa4b2655fd07063a2251d0cff4c87
Plebfoot <toxny.vaneck@gmail.com>
2023-03-11 04:32:51 +0100

    Welkom tekst

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,24 +1,10 @@
 @extends('layouts.admin_layouts.header')
 
 @section('content')
-@csrf
-<div class="test">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    @csrf
+    <div class="test">
+        <div class="row justify-content-center">
+            <h1 style="text-align: center; padding-top: 10px;">Welkom {{ Auth::user()->name }}</h1>
         </div>
     </div>
-</div>
 @endsection
