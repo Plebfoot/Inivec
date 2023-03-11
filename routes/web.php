253e7d79b6bca9f33870ee1dd8199116dc4512a0
Plebfoot <toxny.vaneck@gmail.com>
2023-03-11 04:27:38 +0100

    Routes Aangespast

--- routes/web.php
+++ routes/web.php
@@ -17,33 +17,21 @@ use App\Http\Controllers\ProfilesController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*----------------Normal Routes---------------------*/
+
+Route::get('/', function () { return view('welcome'); });
+Route::get('/inschrijven', function () { return view('inschrijven'); });
+Route::get('/artiesten', function () { return view('artiesten'); });
 
 
-Route::get('/inschrijven', function () {
-    return view('inschrijven');
-});
 
-Route::get('/artiesten', function () {
-    return view('artiesten');
-});
-
+/*----------------Artist Routes---------------------*/
 Route::get('/',[ArtistViewController::class, 'Index']);
-
 Route::get('/arsitesten/{username}', [ArtistController::class, 'Index']);
-
 Route::get('/artiesten/{username}', 'App\Http\Controllers\ProfilesController@show')->name('profiles');
 
 
+/*----------------Auth Routes---------------------*/
 Auth::routes();
-
 Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::group(['middleware' => 'auth'], function() {
-
-    Route::get('/instellingen', function() {
-        return view('/users/profile_management');
-    });
-});
+Route::group(['middleware' => 'auth'], function() {Route::get('/instellingen', function() {return view('/users/profile_management'); }); });
