a9f3119fe31dc1b2c008e4bdbfa070dd5726bdd5
Plebfoot <toxny.vaneck@gmail.com>
2023-03-13 14:52:53 +0100

    Changes aan homepagina

--- resources/views/layouts/admin_layouts/header.blade.php
+++ resources/views/layouts/admin_layouts/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -16,13 +16,10 @@
     <link rel="stylesheet" href="{{ asset('assets/css/admin/nav.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/font-awesome/css/font-awesome.min.css') }}">
 
-
-
-
 </head>
 <div class="hero">
     <nav>
-        @php($profile_img = auth()->user()->profile_img);
+        @php($profile_img = Auth::user()->profile_img);
         <a href="/" class="logo">Inivec</a>
         <ul>
             <li><a href="">Over ons</a></li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,18 @@
             <nav>
                 <h1 class="logo"><a href="/">Inivec</a></h1>
                 @if (Auth::check())
-                <ul>
-                    <li><a href="#">Óver ons</a></li>
-                    <li><a href="#">Contact ons</a></li>
-                </ul>
+                    <ul>
+                        <li><a href="#">Óver ons</a></li>
+                        <li><a href="#">Contact ons</a></li>
+                    </ul>
                     <a href="/home" style="text-decoration: none;"><button type="button" class="btn"
                             style="text-decoration: none;">Dashboard</button></a>
                 @else
-                <ul>
-                    <li><a href="#">Óver ons</a></li>
-                    <li><a href="#">Contact ons</a></li>
-                    <li><a href="/inschrijven">Inschrijven</a></li>
-                </ul>
+                    <ul>
+                        <li><a href="#">Óver ons</a></li>
+                        <li><a href="#">Contact ons</a></li>
+                        <li><a href="/inschrijven">Inschrijven</a></li>
+                    </ul>
                     <a href="/login" style="text-decoration: none;"><button type="button" class="btn"
                             style="text-decoration: none;">Login</button></a>
                 @endif
@@ -36,72 +36,84 @@
             </div>
             <div class="header-content">
                 <h2>Willekeurige artiest</h2>
-                    <div class="artist-showcase">
-                        <div class="showcase-image"><a href="{{ route('profiles', $artist->name) }}"><img src="{{ asset("storage/$artist->profile_img") }} "></a></div>
-                        <div class="showcase-info">
-                            <a href="{{ route('profiles', $artist->name) }}" style="text-decoration: none; color:black;" ><h3>{{ $artist->name }}</h3></a>
-                            <caption>{{ $artist->tags }}</caption>
-                            <div class="rating">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($i <= $artist->rating)
-                                        <span class="fa fa-star checked"></span>
-                                    @else
-                                        <span class="fa fa-star"></span>
-                                    @endif
-                                @endfor
-                            </div>
-                            <span class="tags">{{ $artist->music_tags }}</span> </br>
-                            <span class="price">€{{ $artist->price_range }}</span>
+                <div class="artist-showcase">
+                    <div class="showcase-image"><a href="{{ route('profiles', $artist->name) }}"><img
+                                src="{{ asset("storage/$artist->profile_img") }} "></a></div>
+                    <div class="showcase-info">
+                        <a href="{{ route('profiles', $artist->name) }}" style="text-decoration: none; color:black;">
+                            <h3>{{ $artist->name }}</h3>
+                        </a>
+                        <caption>{{ $artist->tags }}</caption>
+                        <div class="rating">
+                            @for ($i = 1; $i <= 5; $i++)
+                                @if ($i <= $artist->rating)
+                                    <span class="fa fa-star checked"></span>
+                                @else
+                                    <span class="fa fa-star"></span>
+                                @endif
+                            @endfor
                         </div>
-                        <div class="video">
-                            <img src="{{ asset('assets/imgs/band.jpg') }}">
-                        </div>
+                        <span class="tags">{{ $artist->music_tags }}</span> </br>
+                        <span class="price">€{{ $artist->price_range }}</span>
                     </div>
-            </div>
-        </div>
-    </div>
-    <div class="section-regular neutral-100">
-        <div class="container-regular">
-            <div data-w-id="37957543-e9bd-2ee5-9d77-68c8ee874409"
-                style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); opacity: 1; transform-style: preserve-3d;"
-                class="title-wrap-row">
-                <div class="title-wrap-left">
-                    <div class="margin-bottom-16">
-                        <h3>De vier beste genres</h3>
+                    <div class="video">
+                        <img src="{{ asset('assets/imgs/band.jpg') }}">
                     </div>
-                    <p class="paragraph-regular text-gray-600">We hebben nog heel veel meer genres!</p>
                 </div>
             </div>
         </div>
-
     </div>
-    <div class="container_artists">
-        <div class="arists_button">
+    <div class="section">
+        <div class="section-regular neutral-100">
+            <div class="container-regular">
+                <div data-w-id="37957543-e9bd-2ee5-9d77-68c8ee874409"
+                    style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); opacity: 1; transform-style: preserve-3d;"
+                    class="title-wrap-row">
+                    <div class="title-wrap-left">
+                        <div class="margin-bottom-16">
+                            <h3>De vier beste genres</h3>
+                        </div>
+                        <p class="paragraph-regular text-gray-600">We hebben nog heel veel meer genres!</p>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="container_artists">
+
+                <div class="imgs">
+                    <a class="type-item darken">
+                        <img class="type-img img-responsive center-block" alt="Boek een band"
+                            src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
+                    </a>
+                </div>
+                <div class="imgs">
+                    <a class="type-item darken" href="#">
+                        <img class="type-img img-responsive center-block" alt="Boek een band"
+                            src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
+                    </a>
+                </div>
+                <div class="imgs">
+                    <a class="type-item darken" href="#">
+                        <img class="type-img img-responsive center-block" alt="Boek een band"
+                            src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
+                    </a>
+                </div>
+                <div class="imgs">
+                    <a class="type-item darken" href="#">
+                        <img class="type-img img-responsive center-block" alt="Boek een band"
+                            src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
+                    </a>
+                </div>
+                {{-- <div class="arists_button">
             <a href="/artiesten" style="text-decoration: none;"><button class="btn">Zie alle artiesten</button></a>
+        </div> --}}
+            </div>
         </div>
-        <div class="imgs">
-            <a class="type-item darken">
-                <img class="type-img img-responsive center-block" alt="Boek een band"
-                    src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
-            </a>
-        </div>
-        <div class="imgs">
-            <a class="type-item darken" href="#">
-                <img class="type-img img-responsive center-block" alt="Boek een band"
-                    src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
-            </a>
-        </div>
-        <div class="imgs">
-            <a class="type-item darken" href="#">
-                <img class="type-img img-responsive center-block" alt="Boek een band"
-                    src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
-            </a>
-        </div>
-        <div class="imgs">
-            <a class="type-item darken" href="#">
-                <img class="type-img img-responsive center-block" alt="Boek een band"
-                    src="https://dz8pdz0wfluv5.cloudfront.net/production/pictures/images/000/047/515/original/bands.png?1644923887">
-            </a>
+    </div>
+    <div class="info">
+        <div class="infotext"> 
+            <h2>Waarom kiezen voor Inivec?</h2>
         </div>
     </div>
     </div>
