46edf7365cdb162ad9f8266c44c0d9f7b431f69f
Tony van eck <33572052+Plebfoot@users.noreply.github.com>
2023-04-02 15:47:35 +0200

    Update welcome.blade.php

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,6 +3,10 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
 <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css">
+  <meta content="Inivec" property="og:title" />
+    <meta content="Maak je feest legendarisch met onze artiesten!" property="og:description" />
+    <meta content="https://www.inivec.nl" property="og:url" />
+    <meta content="#43B581" data-react-helmet="true" name="theme-color" />
 
 @section('content')
     <!-- Navbar sectie -->
